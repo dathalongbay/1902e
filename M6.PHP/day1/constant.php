6aed1c9729196bbd8dcc0b161e37278621b440d1
tuanlabophp <nguyen.duc.anh.tuan@framgia.com>
2019-05-15 20:07:23 +0700

    111

---
+++ M6.PHP/day1/constant.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<pre>
+    Hằng số dùng để chứa thông tin
+    khác với biến trong quá trình chạy thì biến sẽ thay đổi giá trị
+còn hằng số thì không giá trị
+    Khai báo hằng constant : define("tên hằng số", "giá trị của hằng số");
+    Quy tắc khai báo tên hằng số
+Khác với biến thì cần $ bắt đầu
+Hằng số chỉ nên chứa chữ và số
+Hằng số thì nên viết hoa
+</pre>
+
+<?php
+define("USERNAME", "Nguyễn tuấn anh");
+define("USERID", "12345678");
+echo "<br>" . USERNAME;
+echo "<br>" . USERID;
+?>
+
+</body>
+</html>
