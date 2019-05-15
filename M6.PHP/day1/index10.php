612def17ad0834ed3acb725eedecbdfec9b1e51b
tuanlabophp <nguyen.duc.anh.tuan@framgia.com>
2019-05-15 20:40:56 +0700

    111

--- M6.PHP/day1/index10.php
+++ M6.PHP/day1/index10.php
@@ -9,14 +9,20 @@
     <pre>
         - strlen() đếm số ký tự trong 1 chuỗi
         - str_word_count() đếm số chữ trong 1 chuỗi
-        - strpos() tìm kiếm sự có mặt của 1 chuỗi con trong 1 chuỗi mẹ
+        - strpos("chuỗi mẹ", "chuỗi con") tìm kiếm sự có mặt của 1 chuỗi con trong 1 chuỗi mẹ
         sẽ trả false nếu không tìm được
         trả về vị trí bắt đầu của chuỗi con trong chuỗi mẹ
         - str_replace() thay thế chuỗi 1 bằng chuỗi 2 trong 1 chuỗi mẹ
+        - var_dump($bien) : hàm này được dùng để debug xem cấu trúc của biên thuộc kiểu
+        dữ liệu gì và chứa giá trị nào
     </pre>
 
     <?php
-    //strpos();
+    $find1 = strpos("thanh pho ha noi chao mung quy khach", "ha noi");
+    var_dump($find1);
+    echo "<br>";
+    $find1 = strpos("thanh pho ha noi chao mung quy khach", "da nang");
+    var_dump($find1);
     ?>
 
 </body>
