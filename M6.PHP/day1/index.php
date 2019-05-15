5834880e8954b3c0b1f8cda1a93869ed4616b783
tuanlabophp <nguyen.duc.anh.tuan@framgia.com>
2019-05-15 18:54:57 +0700

    111

---
+++ M6.PHP/day1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1>Cú pháp của php</h1>
+    <h1>Câu lệnh echo sẽ xuất dữ liệu ra màn hình</h1>
+    <h1>Các lệnh php phải kết thúc bằng dấu ;</h1>
+    <?php echo "chao php"; ?>
+</body>
+</html>
