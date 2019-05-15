e1222f226a2e722de5e680ac9e3fc22c6a9207e4
tuanlabophp <nguyen.duc.anh.tuan@framgia.com>
2019-05-15 20:27:41 +0700

    111

---
+++ M6.PHP/day1/funstring1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1>Các hàm xử lý chuỗi trong php</h1>
+    <pre>
+        - strlen() đếm số ký tự trong 1 chuỗi
+        - str_word_count() đếm số chữ trong 1 chuỗi
+        - strpos() tìm kiếm sự có mặt của 1 chuỗi con trong 1 chuỗi mẹ
+        sẽ trả false nếu không tìm được
+        trả về vị trí bắt đầu của chuỗi con trong chuỗi mẹ
+        - str_replace() thay thế chuỗi 1 bằng chuỗi 2 trong 1 chuỗi mẹ
+    </pre>
+
+    <?php
+    //strpos();
+    ?>
+
+</body>
+</html>
