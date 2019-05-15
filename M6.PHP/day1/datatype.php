0fa43cc0da52f153e8bca7551df06fe4218df168
tuanlabophp <nguyen.duc.anh.tuan@framgia.com>
2019-05-15 19:56:37 +0700

    111

---
+++ M6.PHP/day1/datatype.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <pre>
+        Các kiểu dữ liệu trong php :
+        kiểu số nguyên int
+        kiểu số thực float
+        kiểu chuỗi string
+        kiểu null
+        kiểu boolean ( true or false )
+        kiểu đối tượng
+        kiểu mảng
+    </pre>
+    <h1>Kiểu số nguyên</h1>
+    <?php $a = 5; ?>
+<h1>Kiểu số thực</h1>
+<?php $b = 1.5; ?>
+<h1>Kiểu chuỗi</h1>
+<?php $c = "chào php"; ?>
+<h1>Kiểu boolean</h1>
+<?php $d1 = $d = $e = false; ?>
+<?php $f1 = $f = $g = true; ?>
+<h1>Kiểu mảng</h1>
+<?php $mang = array("a", "b", "c", "d"); ?>
+<h1>Kiểu null</h1>
+<?php $n = null; ?>
+</body>
+</html>
