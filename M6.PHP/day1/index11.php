fb0a5b3e8b8f7f481201a6ad73162898c90a2ab7
tuanlabophp <nguyen.duc.anh.tuan@framgia.com>
2019-05-15 20:50:35 +0700

    111

---
+++ M6.PHP/day1/index11.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1>Các hàm xử lý chuỗi trong php</h1>
+    <pre>
+        - str_replace("chuỗi cần tìm và bị thay thế", "chuỗi thay thế", "chuỗi mẹ") thay thế chuỗi 1 bằng chuỗi 2 trong 1 chuỗi mẹ
+        - var_dump($bien) : hàm này được dùng để debug xem cấu trúc của biên thuộc kiểu
+        dữ liệu gì và chứa giá trị nào
+    </pre>
+
+    <?php
+    echo str_replace("ha noi", "da nang", "chao mung ban den tp ha noi");
+    ?>
+
+</body>
+</html>
