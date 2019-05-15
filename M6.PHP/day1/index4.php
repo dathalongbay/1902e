9d1691f9cbf2e4cf1a114a38cebf15c29f034078
tuanlabophp <nguyen.duc.anh.tuan@framgia.com>
2019-05-15 19:35:03 +0700

    111

---
+++ M6.PHP/day1/index4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1>Biến trong php</h1>
+    <h1>Biến là đối tượng , thứ dùng để lưu trữ thông tin và thay đổi giá trị trong
+    quá trình chạy</h1>
+    <h1>Biến trong php bắt đầu bằng $</h1>
+    <h1>= là phép gán vế bên phải sang bên trái</h1>
+    <pre>
+        Quy tắc khai báo tên biến
+        thứ nhất tên biến không được có dấu cách
+        không được có dấu tiếng việt
+        thứ 2 tên biến chỉ được chứa các chữ cái và chữ số và _
+        thứ 3 tên biến phải bắt đầu bằng 1 chữ cái
+    </pre>
+    <?php
+    $tensinhvien = "nguyen van an";
+    echo $tensinhvien;
+    ?>
+
+</body>
+</html>
