f277d0904d76d72c3893507a8773850107249813
tuanlabophp <nguyen.duc.anh.tuan@framgia.com>
2019-05-15 18:59:28 +0700

    111

--- M6.PHP/day1/index.php
+++ M6.PHP/day1/index.php
@@ -8,6 +8,9 @@
     <h1>Cú pháp của php</h1>
     <h1>Câu lệnh echo sẽ xuất dữ liệu ra màn hình</h1>
     <h1>Các lệnh php phải kết thúc bằng dấu ;</h1>
+    <h1>Mặc định khi vào trong folder trong htdocs thì trình duyệt nó sẽ ưu tiên chạy
+    file index.html . Nếu không tôn tại file index.html thì nó sẽ tìm file index.php</h1>
+    <h1>file abc.php : localhost:port/tên thư mục/tên file kèm đuôi php</h1>
     <?php echo "chao php"; ?>
 </body>
 </html>
