9faa95ed852f3ab96a3e813f37306562d78d3803
dathalongbay <dathalongbay@gmail.com>
2019-05-29 19:13:39 +0700

    1

---
+++ M6.PHP/day4/index3.php
@@ -0,0 +1,7 @@
+<?php
+
+$arr1 = array(1,2,3,7,9,11);
+
+// Đếm số phần tử của mảng này
+// sử dụng hàm count để đếm
+echo count($arr1);
---
+++ M6.PHP/day4/index4.php
@@ -0,0 +1,17 @@
+<?php
+
+$arr1 = array(24,7,28,100,1,7,9);
+
+/**
+ * Sắp xếp lại mảng theo thứ tự tăng dần
+ * sử dụng hàm sort();
+ */
+echo "<pre>";
+print_r($arr1);
+echo "</pre>";
+
+sort($arr1);
+
+echo "<pre>";
+print_r($arr1);
+echo "</pre>";
