e1797bce9f9937185fa416163ae891a86ef3add2
dathalongbay <dathalongbay@gmail.com>
2019-06-02 18:51:52 +0700

    1

--- M6.PHP/day5/index1.php
+++ M6.PHP/day5/index1.php
@@ -7,10 +7,14 @@
 <body>
 Áp dụng kiến thức $_POST và $_GET xây dựng 1 form nhập
 chiều cao theo mét và cân năng theo kg để tính ra chỉ số BMI và
-tính toán xếp loại theo các chỉ số gầy béo bệnh tật theo link sau
+tính
+
+toán xếp loại theo các chỉ số gầy béo bệnh tật theo link sau
 https://news.zing.vn/5-cong-thuc-don-gian-do-chi-so-bmi-post669580.html
 đọc lại bài viết nếu cần thiết
 https://toidicode.com/phuong-thuc-get-va-post-trong-php-64.html
+
+
 </body>
 </html>
 
---
+++ M6.PHP/day5/index2.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+https://toidicode.com/include-includeonce-require-requireonce-trong-php-66.html
+</body>
+</html>
