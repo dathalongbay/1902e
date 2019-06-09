a74507b7d39fe2e55a513a78188fb4c06a8e4f8c
bacnv <nguyenvanbac33@gmail.com>
2019-06-09 20:45:10 +0700

    111

--- M9.CRUD/create.php
+++ M9.CRUD/create.php
@@ -8,6 +8,74 @@
 
 </head>
 <body>
+<?php
+// nạp file kết nối CSDL
+include_once "config.php";
+
+$name = "";
+$address = "";
+$salary = "";
+
+/**
+ * Kiểm tra xem có dữ liệu submit đi hay không
+ * !empty($_POST) có nghĩa là không rỗng tức là có dữ liệu trong mảng này
+ * isset($_POST) dùng để kiểm tra biến có tồn tại hay không
+ */
+if (isset($_POST) && !empty($_POST)) {
+
+    /**
+     * Tạo ra 1 biến để check lỗi mặc định là rỗng
+     */
+    $errors = array();
+
+    /**
+     * !isset($_POST["name"]) => không tồn tại
+     *  empty($_POST["name"]) => rỗng
+     */
+    if (!isset($_POST["name"]) || empty($_POST["name"])) {
+        $errors[] = "Tên nhân viên không hợp lệ";
+    }
+
+    if (!isset($_POST["address"]) || empty($_POST["address"])) {
+        $errors[] = "Địa chỉ nhân viên không hợp lệ";
+    }
+
+    if (!isset($_POST["salary"]) || empty($_POST["salary"])) {
+        $errors[] = "Lương nhân viên không hợp lệ";
+    }
+
+    /**
+     * $errors rỗng tức là không có lỗi
+     */
+    if (empty($errors)) {
+        $name = $_POST['name'];
+        $address = $_POST['address'];
+        $salary = $_POST['salary'];
+
+        $sqlInsert = "INSERT INTO employees (name, address, salary) VALUES ('$name', '$address', $salary)";
+
+        // Thực hiện câu SQL
+        $result = $connection->query($sqlInsert);
+
+        if ($result == true) {
+            echo "<div class='alert alert-success'>
+Thêm mới nhân viên thành công ! <a href='index.php'>Trang chủ</a>
+</div>";
+        } else {
+            echo "<div class='alert alert-danger'>
+Thêm mới nhân viên thất bại !
+</div>";
+        }
+    }else{
+        /**
+         * Chuyển mảng $errors thành chuỗi = hàm implode()
+         */
+        $errors_string = implode("<br>", $errors);
+        echo "<div class='alert alert-danger'>$errors_string</div>";
+    }
+}
+?>
+
 
 <div class="container">
     <div class="row">
@@ -16,15 +84,15 @@
             <form name="create" action="" method="post">
                 <div class="form-group">
                     <label>Tên nhân viên:</label>
-                    <input type="text" name="name" class="form-control">
+                    <input type="text" name="name" class="form-control" value="<?php echo $name ?>">
                 </div>
                 <div class="form-group">
                     <label>Địa chỉ nhân viên:</label>
-                    <input type="text" name="address" class="form-control">
+                    <input type="text" name="address" class="form-control" value="<?php echo $address ?>">
                 </div>
                 <div class="form-group">
                     <label>Lương nhân viên:</label>
-                    <input type="text" name="salary" class="form-control">
+                    <input type="text" name="salary" class="form-control" value="<?php echo $salary ?>">
                 </div>
 
                 <button type="submit" class="btn btn-primary">tạo nhân viên</button>
