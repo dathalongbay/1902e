c747468788c9bbe8b6539f570ec3dc06d4ee2967
bacnv <nguyenvanbac33@gmail.com>
2019-06-16 18:45:48 +0700

    111

--- M10.OOP/oop1/index2.php
+++ M10.OOP/oop1/index2.php
@@ -9,3 +9,33 @@
  * và trong method này sẽ khởi tạo kết nối CSDL và gán kết nối cho thuộc tính connection
  *
  */
+
+class Database {
+
+    public $connection;
+
+    public $ipDatabase;
+
+    public $userDatabase;
+
+    public $passDatabase;
+
+    public $databaseName;
+
+
+    public function __construct($ipDatabase, $userDatabase, $passDatabase, $databaseName)
+    {
+        $this->ipDatabase = $ipDatabase;
+        $this->userDatabase = $userDatabase;
+        $this->passDatabase = $passDatabase;
+        $this->databaseName = $databaseName;
+        $this->connection = new mysqli($ipDatabase, $userDatabase, $passDatabase, $databaseName);
+    }
+
+
+    public function disconnect() {
+        mysqli_close($this->connection);
+    }
+
+
+}
