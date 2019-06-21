be5eed9b383e3d3cd3a34eae99ff559ed2c0ddc2
dathalongbay <dathalongbay@gmail.com>
2019-06-21 20:18:02 +0700

    1111

--- M11.MVC/mvc/mvc/controllers/EmployeeController.php
+++ M11.MVC/mvc/mvc/controllers/EmployeeController.php
@@ -1,6 +1,8 @@
 <?php
 namespace MVC\Controllers;
 
+use MVC\Models\EmployeeModel;
+
 class EmployeeController {
 
 
@@ -9,6 +11,10 @@ class EmployeeController {
      */
     public function index() {
 
+        $model = new EmployeeModel();
+
+        $result = $model->getAll();
+
         // trả về view cho phần hiển thị
         include_once "mvc/views/index.php";
     }
@@ -16,6 +22,20 @@ class EmployeeController {
 
     public function create() {
 
+        if (isset($_POST) && !empty($_POST)) {
+
+            $model = new EmployeeModel();
+
+            $result = $model->insert($_POST);
+
+            if ($result == true) {
+                header("Location: index.php?controller=employee&action=index");
+                exit;
+            }
+
+            echo "<br> Lưu dữ liệu không thành công";
+
+        }
 
         // trả về view cho phần hiển thị
         include_once "mvc/views/create.php";
@@ -23,14 +43,53 @@ class EmployeeController {
 
     public function edit() {
 
+        if (isset($_POST) && !empty($_POST)) {
 
-        // trả về view cho phần hiển thị
-        include_once "mvc/views/edit.php";
+            $model = new EmployeeModel();
+
+            $result = $model->update($_POST);
+
+            if ($result == true) {
+                header("Location: index.php?controller=employee&action=index");
+                exit;
+            }
+
+            echo "<br> Cập nhật dữ liệu không thành công";
+        }
+
+        // lấy dữ liệu
+
+        if (isset($_GET["id"])) {
+
+            $model = new EmployeeModel();
+
+            $row = $model->getRow($_GET["id"]);
+
+            // trả về view cho phần hiển thị
+            include_once "mvc/views/edit.php";
+        }
+
+        echo "URL không hợp lệ";
+        exit;
+
     }
 
 
     public function delete() {
 
+        if (isset($_POST) && isset($_POST["id"])) {
+
+            $model = new EmployeeModel();
+
+            $result = $model->delete($_POST["id"]);
+
+            if ($result == true) {
+                header("Location: index.php?controller=employee&action=index");
+                exit;
+            }
+
+            echo "<br> Xóa dữ liệu không thành công";
+        }
 
         // trả về view cho phần hiển thị
         include_once "mvc/views/delete.php";
