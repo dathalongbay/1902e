3c38e3787e34c41d204bf81f04b3e6bdb086843e
uet.nguyenduc <uet.nguyenduc@gmail.com>
2019-07-14 20:47:08 +0700

    111

--- M12.Laravel/blog1/app/Http/Controllers/Backend/ProductController.php
+++ M12.Laravel/blog1/app/Http/Controllers/Backend/ProductController.php
@@ -8,4 +8,33 @@ use App\Http\Controllers\Controller;
 class ProductController extends Controller
 {
     //
+
+    /**
+     * Liệt kê các sản phẩm
+     */
+    public function index() {
+
+        return view("simpleadmin.product.index");
+    }
+
+    /**
+     * tạo view tạo mới sản phẩm
+     */
+    public function create() {
+        return view("simpleadmin.product.create");
+    }
+
+    /**
+     * trả về view sửa sản phẩm
+     */
+    public function edit() {
+        return view("simpleadmin.product.edit");
+    }
+
+    /**
+     * trả về view xóa sản phẩm
+     */
+    public function delete() {
+        return view("simpleadmin.product.delete");
+    }
 }
---
+++ M12.Laravel/blog1/resources/views/simpleadmin/product/delete.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Giang Vien T3H
+ * Date: 7/14/2019
+ * Time: 8:45 PM
+ */
